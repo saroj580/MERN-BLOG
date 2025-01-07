ef3b31e38f715b0d0b896317723b6ba341e6525d
Saroj <chaudharysaroj819@gmail.com>
2025-01-07 21:20:06 +0530

    add a middleware and function to handle error

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,13 +1,14 @@
 import User from '../model/user.model.js';
 import bcryptjs from 'bcryptjs';
+import { errorHandler } from '../utils.js/errorHandler.js';
 // installing bcryptjs using 'npm i bcryptjs'
 // bcrypt is used to hash i.e hide the user password in the database
 
-export const signup = async (req, res) => {
+export const signup = async (req, res, next) => {
     
     const { username, email, password } = req.body;
     if (!username || !email || !password || username === '' || email === '' || password === '') {
-        return res.status(400).json({ message: "All fields are required" });
+        new(errorHandler(400, "All fields are required"));
     }
 
     //hashing the password
@@ -22,7 +23,7 @@ export const signup = async (req, res) => {
         await newUser.save();
         res.json("Signup success");
     } catch (err) {
-        res.status(500).json({message: err.message});
+        next(err);
     }
     //after getting the information from the user we are saving it to the database
 }
--- api/index.js
+++ api/index.js
@@ -34,6 +34,17 @@ mongoose.connect(process.env.MONGO_URI)
 app.use('/api/user', userRoutes)
 app.use('/api/auth', authRoutes)
 
+app.use((err, req, res, next) => {
+    const statusCode = err.statusCode || 500;
+    const message = err.message || "Internal Server Error";
+    res.status(statusCode).json({
+           success: false,
+        statusCode,
+        message
+    })
+     
+})
+
 
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`)
