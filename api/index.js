16525112f2e7a91c308839fae8c1341cad66697b
Saroj <chaudharysaroj819@gmail.com>
2025-01-07 15:44:01 +0530

    connect to the database

--- api/index.js
+++ api/index.js
@@ -1,7 +1,21 @@
-import express from 'express';
+import express from 'express'
+import mongoose from 'mongoose'
+import dotenv from 'dotenv'
 const app = express();
 const port = 8000;
 
+// we have install package call dotenv to use .env file
+//it is use to hide the sensitive information like database url, password etc
+//initialize the dotenv
+dotenv.config()
+
+//connect to the database
+mongoose.connect(process.env.MONGO_URI)
+    .then(() => {
+    console.log("MongoDB connected")
+    }).catch((err) => {
+    console.log(err)
+})
 
 app.get('/', (req, res) => {
     res.send('Hello World!')
