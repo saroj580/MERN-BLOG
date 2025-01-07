8e7a9b79ed99c71337f10450526c44ccd5292c5a
Saroj <chaudharysaroj819@gmail.com>
2025-01-08 01:44:25 +0530

    add functionality to singUp page

--- Client/src/pages/SignUp.jsx
+++ Client/src/pages/SignUp.jsx
@@ -1,8 +1,54 @@
-import {Button, Label, TextInput } from 'flowbite-react'
-import React from 'react'
-import { Link } from 'react-router-dom'
+import {Button, Label, TextInput, Alert, Spinner } from 'flowbite-react'
+import React, { useState } from 'react'
+import { Link, useNavigate } from 'react-router-dom'
 
 export default function SignUp() {
+  const [formdata, setFormData] = useState({})
+  const [errorMessage, setErrorMessage] = useState(null)
+  const [loading, setLoading] = useState(false)
+  const navigate = useNavigate()
+
+
+  const handleChange = (e) => {
+    setFormData({ ...formdata, [e.target.id]: e.target.value.trim() })
+    //while setting the data  we are keeping the previous data usinf '...formData' i.e spread operator
+  }
+  console.log(formdata);
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    //while sumbitting the form the page reload so we don't want that therefore we are preventing that using preventDefault() method
+    if (!formdata.username || !formdata.email || !formdata.password) {
+      return setErrorMessage('please fill out all fields')
+    }
+    try { 
+      setLoading(true);
+      //if there is the error from the previous request then we are setting the error message to null i.e empty
+      setErrorMessage(null);
+      const res = await fetch('/api/auth/signup', {
+        //here we are sending the data to the server
+        method: 'POST',
+        //here we are sending the data in json format
+        headers: { 'Content-Type': 'application/json' },
+        //converting the json data to string for the readable format
+        body: JSON.stringify(formdata)
+      })
+      const data = await res.json();
+      //here we are checking if the username is same then throw error message
+      if (data.success === false) {
+        return setErrorMessage(data.message)
+      }
+      setLoading(false);
+      if (res.ok) {
+        navigate('/sign-in')
+      }
+    } catch (error) {
+      setErrorMessage(error.message)
+      setLoading(false);
+    }
+  }
+  
+
   return (
     <div className='min-h-screen mt-20'>
       <div className='flex p-3 max-w-3xl mx-auto flex-col md:flex-row md:items-center gap-5'>
@@ -16,36 +62,50 @@ export default function SignUp() {
         </div>
         {/* right side */}
         <div className='flex-1'>
-          <form className='flex flex-col gap-4' >
+          <form className='flex flex-col gap-4' onSubmit={handleSubmit} >
             <div>
               <Label value='Your username' />
               <TextInput 
                 type='text'
                 placeholder='Username'
-                id='username' />
+                id='username' onChange={handleChange}/>
             </div>
             <div>
               <Label value='Your email' />
               <TextInput 
-                type='text'
+                type='email'
                 placeholder='name@example.com'
-                id='email' />
+                id='email' onChange={handleChange}/>
             </div>
             <div>
               <Label value='Your password' />
               <TextInput 
-                type='text'
+                type='password'
                 placeholder='Password'
-                id='password' />
+                id='password' onChange={handleChange}/>
             </div>
-            <Button gradientDuoTone='purpleToPink' type='submit'>
-              Sign Up
+            <Button gradientDuoTone='purpleToPink' type='submit' disabled={loading}>
+              {
+                loading ? (
+                  <>
+                    <Spinner size='sm' />
+                    <span className='pl-3'>Loading...</span>
+                    </>
+                ) : 'Sign Up'
+              } 
             </Button> 
           </form>
           <div className='flex gap-2 text-sm mt-5'>
             <span>Have an Account?</span>
             <Link to='/sign-in' className='text-blue-500'>Sign In</Link>
           </div>
+          {
+            errorMessage && (
+              <Alert className='mt-5' color='failure'>
+                {errorMessage}
+              </Alert>
+            )
+          }
         </div>
       </div>
     </div>
--- Client/vite.config.js
+++ Client/vite.config.js
@@ -3,5 +3,14 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vite.dev/config/
 export default defineConfig({
+  //connecting the fronted with server
+  server: {
+    proxy: {
+      '/api': {
+        target: 'http://localhost:8000',
+        secure : false
+      },
+    },
+  },
   plugins: [react()],
 })
